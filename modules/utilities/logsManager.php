4b154b0af389d0869eb301e5ff5d4a3d01b2a605
IbraheemAredda <56740646+IbraheemAredda@users.noreply.github.com>
2019-12-29 19:36:07 +0100

    Repairing Logs Manager

--- modules/utilities/logsManager.php
+++ modules/utilities/logsManager.php
@@ -3,10 +3,17 @@
 class LogsManager
 {
     public static $logs = array();
-    public static $path = "../../logs/logs.txt";
+    public static $path = "logs/logs.txt";
+
+    public static function fixPath ()
+    {
+        self::$path = $_SERVER["DOCUMENT_ROOT"] . "/DatabaseManagerAPI/logs/logs.txt";
+    }
 
     public static function read()
     {
+        self::fixPath (); 
+
         $file = fopen(self::$path, "r");
 
         self::$logs = array();
@@ -16,16 +23,30 @@ class LogsManager
 
             if (strlen($row) > 0) {
                 $dataArray = explode(";", $row);
-                array_push(self::$logs, ["user" => $dataArray[0], "database" => $dataArray[1], "table" => $dataArray[2], "event" => $dataArray[3], "id" => $dataArray[4]]);
+                array_push(self::$logs, [
+                    "user" => $dataArray[0], 
+                    "database" => $dataArray[1], 
+                    "table" => $dataArray[2], 
+                    "event" => $dataArray[3], 
+                    "id" => $dataArray[4], 
+                    "time" => $dataArray[5]
+                ]);
             }
-        }
+        } 
+
         fclose($file);
+
+        return self::$logs;
     }
 
     public static function filter($arr)
     {
+        if (count (self::$logs) == 0)
+            self::read(); 
+
         $filteredArray = array();
-        foreach (self::$logs as $logRow) {
+        foreach (self::$logs as $logRow) 
+        {
             $counter = 0;
             foreach ($arr as $filterColumn => $filterValue) {
                 foreach ($logRow as $columnName => $value) {
@@ -44,15 +65,19 @@ class LogsManager
 
     public static function append($arr)
     {
+        self::fixPath (); 
+
         $file = fopen(self::$path, "a+");
 
-        fwrite($file, implode(";", $arr) . "\r\n");
+        fwrite($file, implode(";", $arr));
 
         fclose($file);
     }
 
     public static function clear()
     {
+        self::fixPath (); 
+
         unlink(self::$path);
     }
 }
