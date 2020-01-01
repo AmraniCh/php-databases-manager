d9d276d714ccd465e636b8fad86e956fa99e84c7
IbraheemAredda <psychopathic.psycho.98@gmail.com>
2020-01-01 15:03:00 +0100

    Added Permission Test Units

---
+++ test_units/permission/grant.php
@@ -0,0 +1,7 @@
+<?php
+
+$_POST["type"] = "grant";
+$_POST["user"] = "gyro";
+$_POST["privilege"] = "INSERT";
+
+include "../../modules/handler.php";
---
+++ test_units/permission/register.php
@@ -0,0 +1,7 @@
+<?php
+
+$_POST["type"] = "register";
+$_POST["user"] = "gyro";
+$_POST["pass"] = "123";
+
+include "../../modules/handler.php";
